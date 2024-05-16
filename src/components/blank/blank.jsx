716d1b02eed45675939b5e42a6bf1380bf189972
Brainiac <116993889+Brainitech@users.noreply.github.com>
2024-05-16 22:02:18 +0530

    Update blank.jsx

--- src/components/blank/blank.jsx
+++ src/components/blank/blank.jsx
@@ -5,6 +5,7 @@ function Blank() {
   return (
     <div className="def-cont">
       <h1>This Page is Currently Unavailable</h1>
+      <h2>Availabe: "About","Team","Alumini"</h2>
     </div>
   )
 }
